a06f485342db0f7d4f2f68fa72ffb1e1a978d959
AndyShaw01 <974052646@qq.com>
2024-07-21 23:16:22 +0800

    update

--- Scripts/promptfuzz_focus_attack_challenge_defense.sh
+++ Scripts/promptfuzz_focus_attack_challenge_defense.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
 PYTHON_SCRIPT="./Experiment/run.py"
-PHASE="init"
+PHASE="focus"
 MODE="hijacking"
-NO_MUTATE="True"
+NO_MUTATE="False"
 ALL_DEFENSES="False"
 RETRIEVAL_METHOD="cosine_similarity"
 CLUSTER_NUM=5
 THRESHOLD_COEFFICIENT=0.5
-FEW_SHOT="False"
+FEW_SHOT="True"
 DYNAMIC_ALLOCATE="True"
 FEW_SHOT_NUM=3
 INDEX=0
-MAX_JAILBREAK=5
+MAX_JAILBREAK=1
+MUTATOR_WEIGHTS_HIJACKING=(0.21, 0.23, 0.13, 0.23, 0.2)
+MUTATOR_WEIGHTS_EXTRATION=(0.1, 0.1, 0.4, 0.2, 0.2)
+IS_FINETUNED="True"
+FINE_TUNE_MODEL_PATH="ft:gpt-3.5-turbo-0125:northwestern-university::9LTrBmSM"
 
 # Check if NO_MUTATE, ALL_DEFENSES, FEW_SHOT, and DYNAMIC_ALLOCATE should be set to true
 NO_MUTATE_FLAG=""
 ALL_DEFENSES_FLAG=""
 FEW_SHOT_FLAG=""
 DYNAMIC_ALLOCATE_FLAG=""
+MUTATOR_WEIGHTS=""
+MODEL_PATH=""
+
 
 if [ "$NO_MUTATE" = "True" ]; then
   NO_MUTATE_FLAG="--no_mutate"
@@ -36,9 +43,22 @@ if [ "$DYNAMIC_ALLOCATE" = "True" ]; then
   DYNAMIC_ALLOCATE_FLAG="--dynamic_allocate"
 fi
 
+if [ "$MODE" = "hijacking" ]; then
+  MUTATOR_WEIGHTS=${MUTATOR_WEIGHTS_HIJACKING[@]}
+else
+  MUTATOR_WEIGHTS=${MUTATOR_WEIGHTS_EXTRATION[@]}
+fi
+
+if [ "$IS_FINETUNED" = "True" ]; then
+  MODEL_PATH="--model_path $FINE_TUNE_MODEL_PATH"
+fi
 
 # Set the log path
-LOG_PATH="Logs/${PHASE}/${MODE}/lmi/"
+if [ "$IS_FINETUNED" = "True" ]; then
+    LOG_PATH="Logs/${PHASE}/${MODE}/challenging_defenses_finetuned/"
+else
+    LOG_PATH="Logs/${PHASE}/${MODE}/challenging_defenses_basemodel/"
+fi
 
 # Create the log directory if it does not exist
 mkdir -p "$LOG_PATH"
@@ -47,20 +67,29 @@ mkdir -p "$LOG_PATH"
 run_python_script() {
     local index=$1
     # python -u "$PYTHON_SCRIPT" --index $index --phase $PHASE --mode $MODE $NO_MUTATE_FLAG > "${LOG_PATH}/${index}.log" 2>&1
-    python -u "$PYTHON_SCRIPT" --index $index --max_jailbreak $MAX_JAILBREAK \
-        --phase $PHASE --mode $MODE $NO_MUTATE_FLAG $ALL_DEFENSES_FLAG $FEW_SHOT_FLAG $DYNAMIC_ALLOCATE_FLAG \
+    python -u "$PYTHON_SCRIPT" --index $index \
+        --phase $PHASE \
+        --mode $MODE \
+        $MODEL_PATH \
+        --max_jailbreak $MAX_JAILBREAK \
+        $NO_MUTATE_FLAG \
+        $ALL_DEFENSES_FLAG \
+        $FEW_SHOT_FLAG \
         --retrieval_method $RETRIEVAL_METHOD \
         --cluster_num $CLUSTER_NUM \
+        --few_shot_num $FEW_SHOT_NUM
+        $DYNAMIC_ALLOCATE_FLAG \
         --threshold_coefficient $THRESHOLD_COEFFICIENT \
-        --few_shot_num $FEW_SHOT_NUM > "${LOG_PATH}/${index}.log" 2>&1
+        --mutator_weights $MUTATOR_WEIGHTS> "${LOG_PATH}/${index}.log" 2>&1
     echo "Task $index finished."
 }
 
 # Run the Python script
-for index in {0..150}; do
+for index in {0..29}; do
     run_python_script $index &
     ((index++))
     [ $((index % 8)) -eq 0 ] && wait
 done
+# run_python_script 28
 
 echo "All tasks finished."
