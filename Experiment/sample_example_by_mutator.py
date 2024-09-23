040ef1afad46d508fd207d2af9ec1b6b79409767
AndyShaw01 <974052646@qq.com>
2024-09-23 20:09:22 +0800

    update

--- Experiment/sample_example_by_mutator.py
+++ Experiment/sample_example_by_mutator.py
@@ -15,19 +15,20 @@ def main(args):
     for mutation in mutation_list:
         filtered_df = df_results[df_results['mutation'] == mutation].sort_values(by='success_num', ascending=False)
         filtered_df = filtered_df.drop_duplicates(subset='parent', keep='first')
-        df_examples = df_examples._append(filtered_df.head(20), ignore_index=True)
+        df_examples = df_examples._append(filtered_df.head(args.top_k), ignore_index=True)
     df_examples = df_examples.drop(columns=['success_num'])
     df_examples.to_csv(args.output_file, index=False)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Testing parameters')
-    parser.add_argument('--target_file', type=str, default='./Results/preparation/hijacking/hijacking_evaluation_all_results_add_2.csv',
+    parser.add_argument('--target_file', type=str, default='./Results/preparation/hijacking/all_defenses.csv',
                         help='The file to be evaluated')
     parser.add_argument('--init_file', type=str, default='./Datasets/hijacking_preparation_seed.jsonl',
                         help='The init file to get the index2sample_id')
     parser.add_argument('--output_file', type=str, default='./Results/preparation/hijacking/hijacking_few_shot_example.csv',
                         help='The output file to save the results')
+    parser.add_argument('--top_k', type=int, default=20, help='The number of examples to be saved')
 
     args = parser.parse_args()
 
