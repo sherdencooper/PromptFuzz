6f1f14d4717a893bd24268386a6c1736d05b2f9b
AndyShaw01 <974052646@qq.com>
2024-10-10 12:22:18 +0800

    update for preparation

--- Experiment/get_metric.py
+++ Experiment/get_metric.py
@@ -1,6 +1,21 @@
 import argparse
 import pandas as pd
 import numpy as np
+import glob
+import os
+
+def read_csv_files(path):
+    # Read all the CSV files in the given path
+    #@param path: the path to the CSV files
+    #@return: the dataframe containing the data
+    all_files = glob.glob(os.path.join(path, "*.csv"))
+    df = pd.DataFrame()
+    for file in all_files:
+        file_name = os.path.basename(file).split('.')[0]
+        temp_df = pd.read_csv(file, nrows=1)
+        temp_df['defense_id'] = file_name
+        df = pd.concat([temp_df, df])
+    return df
 
 def get_asr_list(df, parent, defense_num):
     # Get the ASR list for a given parent
@@ -8,7 +23,6 @@ def get_asr_list(df, parent, defense_num):
     #@param parent: the parent ID
     #@param defense_num: the number of defenses
     #@return: the ASR list for the given parent
-
     asr_list = [0] * defense_num
     defense_ids = df[df['parent'] == parent]['defense_id'].tolist()
     for defense_id in defense_ids:
@@ -22,10 +36,13 @@ def get_metric_normal(file_path, method='promptfuzz',topK=5, defense_num=150):
     #@param topK: the number of top defenses to consider for the ensemble ASR
     #@param defense_num: the number of defenses
     #@return: the best ASR, ensemble ASR, and coverage metric for the given file
+    try:
+        df = pd.read_csv(file_path)
+    except:
+        df = pd.read_csv(file_path, on_bad_lines='skip')
+        df = df.dropna()
     
-    df = pd.read_csv(file_path)
-
-    if method == 'humanexpert' or method == 'gcg':
+    if method == 'human_expert' or method == 'gcg':
         unique_parents = df.drop_duplicates(subset=['parent'])['parent'].tolist()
         attack_list = []
         for parent in unique_parents:
@@ -35,6 +52,7 @@ def get_metric_normal(file_path, method='promptfuzz',topK=5, defense_num=150):
             'AttackSuccessList': attack_list,
             'AttackSuccessRate': [sum(attack_list[i]) / defense_num for i in range(len(unique_parents))],
         })
+
     else:
         df_new = pd.DataFrame({'AttackID': df['index'], 
                                'AttackSuccessList': df['results'].apply(lambda x: eval(x)),
@@ -59,8 +77,8 @@ def get_metric_normal(file_path, method='promptfuzz',topK=5, defense_num=150):
 
 def main(args):
     # preprocess the file
-    if args.methods == 'humanexpert' or args.methods == 'gcg':
-        df_aggregate_csv_results = pd.read_csv(args.target_path)
+    if args.method == 'human_expert' or args.method == 'gcg':
+        df_aggregate_csv_results = read_csv_files(args.target_path)
         df_aggregate_csv_results.to_csv(args.aggregate_csv_file_path, index=False)
         args.target_path = args.aggregate_csv_file_path
 
@@ -80,8 +98,8 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Aggregate CSV results')
-    parser.add_argument('--method', choices=['promptfuzz', 'gptfuzz', 'gcg', 'humanexpert'], default='promptfuzz', help='Choose which result of method to process')
-    parser.add_argument('--aggregate_csv_file_path', type=str, help='The path to the aggregate csv file for humanexpert and gcg')
+    parser.add_argument('--method', choices=['promptfuzz', 'gptfuzz', 'gcg', 'human_expert'], default='promptfuzz', help='Choose which result of method to process')
+    parser.add_argument('--aggregate_csv_file_path', type=str, help='The path to the aggregate csv file for human_expert and gcg')
     parser.add_argument('--target_path', type=str, help='The path to the file')
     parser.add_argument('--save_path', type=str, help='The path to save the file')
     parser.add_argument('--defense_num', type=int, default=150, help='The number of defenses')
--- Scripts/analysis_preparation.sh
+++ Scripts/analysis_preparation.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+MODE="hijacking" # extraction
 GET_ATTACK_RANKING_SCRIPT="./Experiment/sample_topN_focus_seed.py"
 SAMPLE_EXAMPLE_BY_MUTATOR_SCRIPT="./Experiment/sample_example_by_mutator.py"
 GET_MUTATOR_WEIGHTS_SCRIPT="./Experiment/get_mutator_weights.py"
 
-TARGET_FILE=""
-INIT_FILE="./Datasets/hijacking_preparation_seed.jsonl"
+TARGET_FILE="./Results/preparation/${MODE}/all_results.csv"
+INIT_FILE="./Datasets/${MODE}_preparation_seed.jsonl"
 
-OUTPUT_FILE_SEED="./Datasets/hijacking_focus_seed.jsonl"
-OUTPUT_FILE_EXAMPLE="./Datasets/hijacking_focus_example.jsonl"
-OUTPUT_FILE_EXAMPLE_MUTATOR=""
+OUTPUT_FILE_SEED="./Datasets/${MODE}_focus_seed.jsonl"
+OUTPUT_FILE_EXAMPLE="./Datasets/${MODE}_few_shot_example.csv"
+OUTPUT_FILE_EXAMPLE_MUTATOR="./Results/preparation/${MODE}/mutator_weights.csv"
 
 # Run the Python script
 python -u "$GET_ATTACK_RANKING_SCRIPT" --target_file $TARGET_FILE --init_file $INIT_FILE --output_file $OUTPUT_FILE_SEED 
@@ -18,5 +19,5 @@ echo "Sampled top N focus seed tasks finished."
 python -u "$SAMPLE_EXAMPLE_BY_MUTATOR_SCRIPT" --target_file $TARGET_FILE --init_file $INIT_FILE --output_file $OUTPUT_FILE_EXAMPLE
 echo "Sampled few-shot example tasks finished."
 
-python -u "$GET_MUTATOR_WEIGHTS_SCRIPT" --target_file $TARGET_FILE --ouptut_file $OUTPUT_FILE_EXAMPLE_MUTATOR
+python -u "$GET_MUTATOR_WEIGHTS_SCRIPT" --target_file $TARGET_FILE --output_file $OUTPUT_FILE_EXAMPLE_MUTATOR
 echo "Get mutator weights finished."
